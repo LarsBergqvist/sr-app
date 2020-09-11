4aae4c92feff68b57bfbcf8e1409f211e2f8f811
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2020-09-11 20:01:36 +0200

    Upgraded to angular@10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChannelsListComponent } from './channels-list/channels-list.component';
 import { HttpClientModule } from '@angular/common/http';
-import { TableModule } from 'primeng-lts/table';
-import { ToolbarModule } from 'primeng-lts/toolbar';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @NgModule({
     declarations: [
