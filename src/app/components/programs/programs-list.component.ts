5570996398969dc1b4648f7170f847b9b7d361aa
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2021-06-20 22:31:13 +0200

    Remember state of ProgramsList

--- src/app/components/programs/programs-list.component.ts
+++ src/app/components/programs/programs-list.component.ts
@@ -1,34 +1,48 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { SelectItem } from 'primeng/api';
+import { Table } from 'primeng/table';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { ShowProgramDetailsMessage } from 'src/app/messages/show-programdetails.message';
 import { ProgramCategory } from 'src/app/models/program-category';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { MessageBrokerService } from 'src/app/services/message-broker.service';
 import { SRApiService } from 'src/app/services/srapi.service';
 import { TranslationService } from 'src/app/services/translation.service';
 import { Program } from '../../models/program';
-import { ProgramDetailsComponent } from './program-details.component';
 
 @Component({
   selector: 'app-programs-list',
   templateUrl: './programs-list.component.html',
   styleUrls: ['../common/datatable-styling.scss', './programs-list.component.scss']
 })
-export class ProgramsListComponent implements OnInit, OnDestroy {
+export class ProgramsListComponent implements OnInit, OnDestroy, AfterViewInit {
   programs: Program[];
   categoryOptions: SelectItem[];
   private unsubscribe$ = new Subject();
-  showOnlyFavs = false;
 
-  @ViewChild(ProgramDetailsComponent) episodesComp: ProgramDetailsComponent;
+  private readonly storageId = 'ProgramsListState';
+  localState = {
+    showOnlyFavs: false,
+    selectedCategory: null,
+    searchString: ''
+  };
+
+  @ViewChild(Table) tableComponent: Table;
+
   constructor(
     private readonly srApiService: SRApiService,
     private readonly translationService: TranslationService,
-    private readonly broker: MessageBrokerService
+    private readonly broker: MessageBrokerService,
+    private readonly storage: LocalStorageService
   ) {}
 
   async ngOnInit() {
+    const oldState = this.storage.get(this.storageId);
+    if (oldState) {
+      this.localState = oldState;
+    }
+
     this.srApiService.programs$.pipe(takeUntil(this.unsubscribe$)).subscribe((values: Program[]) => {
       if (values) {
         this.programs = [];
@@ -45,7 +59,26 @@ export class ProgramsListComponent implements OnInit, OnDestroy {
     });
   }
 
+  ngAfterViewInit(): void {
+    if (this.tableComponent) {
+      if (this.localState.showOnlyFavs) {
+        this.tableComponent.filter(true, 'fav', 'equals');
+      } else {
+        this.tableComponent.filter([true, false], 'fav', 'in');
+      }
+
+      if (this.localState.selectedCategory) {
+        this.tableComponent.filter(this.localState.selectedCategory, 'programcategory.id', 'equals');
+      }
+
+      if (this.localState.searchString) {
+        this.tableComponent.filterGlobal(this.localState.searchString, 'contains');
+      }
+    }
+  }
+
   ngOnDestroy() {
+    this.storage.set(this.storageId, this.localState);
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
   }
@@ -55,8 +88,8 @@ export class ProgramsListComponent implements OnInit, OnDestroy {
   }
 
   onFilterFavClicked(event, dt) {
-    this.showOnlyFavs = event.checked;
-    if (this.showOnlyFavs) {
+    this.localState.showOnlyFavs = event.checked;
+    if (this.localState.showOnlyFavs) {
       dt.filter(true, 'fav', 'equals');
     } else {
       dt.filter([true, false], 'fav', 'in');
