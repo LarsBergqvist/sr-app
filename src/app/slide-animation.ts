8a8a446cc5b1154043141e2ffda023ed42b42379
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2023-11-23 05:00:58 +0100

    Route for episodes/:id instead of sidebar (#56)

--- src/app/slide-animation.ts
+++ src/app/slide-animation.ts
@@ -1,7 +1,7 @@
 import { trigger, transition, style, query, animateChild, animate, group } from '@angular/animations';
 
 export const SlideInAnimation = trigger('routeAnimations', [
-  transition('* => details', [
+  transition('* => *', [
     style({ position: 'relative' }),
     query(
       ':enter, :leave',
@@ -21,29 +21,6 @@ export const SlideInAnimation = trigger('routeAnimations', [
     group([
       query(':leave', [animate('300ms ease-out', style({ left: '100%' }))], { optional: true }),
       query(':enter', [animate('300ms ease-out', style({ left: '0%' }))], { optional: true })
-    ]),
-    query(':enter', animateChild(), { optional: true })
-  ]),
-  transition('details => *', [
-    style({ position: 'relative' }),
-    query(
-      ':enter, :leave',
-      [
-        style({
-          paddingTop: '48px',
-          position: 'absolute',
-          top: '-4px',
-          right: 0,
-          width: '100%'
-        })
-      ],
-      { optional: true }
-    ),
-    query(':enter', [style({ right: '-100%' })], { optional: true }),
-    query(':leave', animateChild(), { optional: true }),
-    group([
-      query(':leave', [animate('300ms ease-out', style({ right: '100%' }))], { optional: true }),
-      query(':enter', [animate('300ms ease-out', style({ right: '0%' }))], { optional: true })
     ]),
     query(':enter', animateChild(), { optional: true })
   ])
